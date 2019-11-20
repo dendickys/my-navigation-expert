ae0b251bc7a824e8fee25d7b4c626210afdbeb2c
dendickys <febri.unquad2014@gmail.com>
2019-11-21 06:51:23 +0700

    codelabs navigation finish

---
+++ app/src/main/java/id/dendickys/mynavigationexpert/DetailCategoryFragment.java
@@ -0,0 +1,53 @@
+package id.dendickys.mynavigationexpert;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailCategoryFragment extends Fragment {
+
+
+    public DetailCategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView tvCategoryName = view.findViewById(R.id.tv_category_name);
+        TextView tvCategoryDescription = view.findViewById(R.id.tv_category_description);
+
+        assert getArguments() != null;
+        String dataName = DetailCategoryFragmentArgs.fromBundle(getArguments()).getName();
+        long dataDescription = DetailCategoryFragmentArgs.fromBundle(getArguments()).getStock();
+
+        tvCategoryName.setText(dataName);
+        tvCategoryDescription.setText("Stock : " + dataDescription);
+
+        Button btnProfile = view.findViewById(R.id.btn_profile);
+        btnProfile.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment, null)
+        );
+    }
+}
